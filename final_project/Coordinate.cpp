82fce07b61c46685733d2f6c04c4237c9790baf0
gahxi0769 <80142744+brianvlee5@users.noreply.github.com>
2022-05-05 01:42:34 +0800

    Update Coordinate.cpp

--- final_project/Coordinate.cpp
+++ final_project/Coordinate.cpp
@@ -14,7 +14,7 @@ void Coordinate::calMapCamera(Map m, AnimeObject mainch)
 	SDL_Rect temp = m.getcamera();
 	printCamX = (mainch.getX() - temp.x);
 	printCamY = (mainch.getY() - temp.y);
-	printf("prtcx: %d  prtcy: %d\n", printCamX, printCamY);
+	
 }
 
 int Coordinate::getpCX()

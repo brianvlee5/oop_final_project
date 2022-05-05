5c0627d1ac8e35fa31a4d2fbe492781716e0e233
gahxi0769 <80142744+brianvlee5@users.noreply.github.com>
2022-05-06 00:01:57 +0800

    Add files via upload

---
+++ final_project/Monster.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <SDL.h>
+#include "SDL2_gfxPrimitives.h"
+#include <stdio.h>
+#include "constants.h"
+#include "SDL_image.h"
+#include "tile.h"
+#include "MonsterAI.h"
+
+
+
+class Monster
+{
+public:
+	Monster(const char* path, int n, SDL_Renderer* ren);
+	Monster(const char* path, int n, SDL_Renderer* ren, Uint8 r, Uint8 g, Uint8 b);
+	void close();
+	void setPosition(int xx, int yy);
+	int getWidth();
+	int getHeight();
+	int getX();
+	int getY();
+	void setVX(int x);
+	void setVY(int y);
+	int getVX();
+	int getVY();
+	//	void draw(SDL_Renderer* renderer);
+	void draw(SDL_Rect, SDL_Rect);
+	void startTimer(Uint32 t);
+	void stopTimer();
+	//	void move();
+	void move();
+	void setdetectCorner();
+	void moveOrNot();
+	bool xRight();
+	bool xLeft();
+	bool yUp();
+	bool yDown();
+	void setJumpFlag(bool f);
+	void AIstart();
+
+private:
+	char path[100];
+	int num;
+	SDL_Texture** texture;
+	SDL_Renderer* renderer;
+	int frame;
+	int width;
+	int height;
+	int x;
+	int y;
+	SDL_TimerID timerID;
+	Uint32 time;
+	static Uint32 changeData(Uint32 interval, void* param);
+	int velX;
+	int velY;
+	int detectCornerX[4][2];
+	int detectCornerY[4][2];
+	bool jumpFlag;
+};

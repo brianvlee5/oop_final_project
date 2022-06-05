253fb47f12d195abdc5cf1bc1ebbaf1b2ceda5df
gahxi0769 <80142744+brianvlee5@users.noreply.github.com>
2022-06-06 00:16:47 +0800

    Add files via upload

--- final_project/MonsterI.h
+++ final_project/MonsterI.h
@@ -7,6 +7,10 @@ public:
     MonsterI(const char* path, int n, SDL_Renderer* ren);
     MonsterI(const char* path, int n, SDL_Renderer* ren, Uint8 r, Uint8 g, Uint8 b);
     void gotKey(AnimeObject2& mainch);
+    void setMchptr(AnimeObject2& mainch);
+    void startAI(Uint32 t);
+    void stopAI();
+
 private:
 
 };
